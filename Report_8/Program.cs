21437db6e5de7ecd0418a6c5cd05d909aa36243e
Chahat Kumar <kchahat20@gmail.com>
2020-06-25 11:50:51 +0530

    Updated :octocat: :four_leaf_clover:

---
+++ Report_8/Program.cs
@@ -0,0 +1,143 @@
+﻿/*
+ *Read 20 files( 10 of 1 MB and 10 of 2 MB) in parallel with 5 buffers of 1MB and
+ * 5 buffers of 2 MB using concurrent append in one file. 		
+ */
+using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Buffers;
+using System.Collections.Concurrent;
+
+using Microsoft.Azure.DataLake.Store;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.Rest.Azure.Authentication;
+
+namespace Report_7
+{   
+    public class Buffering
+    {
+        private const int One_MB = 1050000;
+        private const int Two_MB = 2100000;
+
+        private byte[][] buffer1 = new byte[5][];
+        private byte[][] buffer2 = new byte[5][];
+
+        private ArrayPool<byte> samePool = ArrayPool<byte>.Shared;
+
+        public void SendData(AdlsClient c, int i,BlockingCollection<int> b,string filename, string path)
+        {
+            i = i % 5; 
+            FileInfo f = new FileInfo(path); 
+            long length = f.Length;
+
+            while (length>0)
+            {
+                // sending data via 1 MB buffer if data is less than/equal to 1 MB
+                if (length - One_MB <= 0)
+                {
+                    b.Add(1);
+                    if (buffer1[i] == null)
+                    { 
+                        buffer1[i] = samePool.Rent(One_MB); 
+                    }
+                    b.Take(); 
+                    length = length - buffer1[i].Length;
+                    lock (buffer1[i])
+                    {
+                        using (var file = new FileStream(path, FileMode.Open))
+                        { 
+                            file.Read(buffer1[i], 0, buffer1[i].Length); 
+                            c.ConcurrentAppend(filename, true, buffer1[i] , 0, (int) buffer1[i].Length); 
+                            Array.Clear(buffer1[i], 0, (int) buffer1[i].Length);
+                        }
+                    } 
+                }
+                //sending data via 2 MB buffer if data is more than 1 MB
+                else
+                {
+                    //sending data via 2 MB buffer
+                    b.Add(1);
+                    if (buffer2[i] == null)
+                    { 
+                        buffer2[i] = samePool.Rent(Two_MB); 
+                    }
+                    b.Take(); 
+                    length = length - buffer2[i].Length;
+                    lock (buffer2[i])
+                    {
+                        using (var file = new FileStream(path, FileMode.Open))
+                        { 
+                            file.Read(buffer2[i], 0, buffer2[i].Length); 
+                            c.ConcurrentAppend(filename, true, buffer2[i] , 0, (int) buffer2[i].Length); 
+                            Array.Clear(buffer2[i], 0, (int) buffer2[i].Length);
+                        }
+                    } 
+
+                }
+            }
+               
+        }
+
+        ~Buffering()
+        { 
+          //returning all rented arrays 
+          for (int i = 0; i < 5; i++)
+          { 
+              samePool.Return(buffer1[i]);
+              samePool.Return(buffer2[i]);
+          }
+        }
+    }
+    public class Program
+    {
+
+        private static string applicationId = "35fe4d8f-b30e-40ed-8cdf-fee0216569de";     // Also called client id
+        private static string clientSecret = ".P0KipHqZAj18k-.t8_-WN4v~90.Jst08h";
+        private static string tenantId = "72f988bf-86f1-41af-91ab-2d7cd011db47";
+        private static string adlsAccountFQDN = "marstest.azuredatalakestore.net";   // full account FQDN, not just the account name like example.azure.datalakestore.net
+                                                                                  
+        public static void Main(string[] args)
+        {
+            var creds = new ClientCredential(applicationId, clientSecret);
+            var clientCreds = ApplicationTokenProvider.LoginSilentAsync(tenantId, creds).GetAwaiter().GetResult();
+            // Create ADLS client object
+            AdlsClient client = AdlsClient.CreateClient(adlsAccountFQDN, clientCreds);
+            
+            var obj = new Buffering();
+
+            try
+            {
+                //file that wil be created and appended
+                string filename = @"Report_8.txt";
+                string[] path = new string[20];
+                BlockingCollection<int> b = new BlockingCollection<int>(1);
+
+                Parallel.For(0, 20, i => {
+
+                    if (i < 10)
+                    {
+                        path[i] = @"C:\Users\kchah\OneDrive\Desktop\InputFiles\1MB\" + (i + 1) + ".txt";
+                    }
+                    else
+                    {
+                        path[i] = @"C:\Users\kchah\OneDrive\Desktop\InputFiles\2MB\" + ((i%10) + 1) + ".txt";
+                    }
+
+                    obj.SendData(client,i,b, filename, path[i]);
+
+                });
+
+            }
+            catch (AdlsException e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.WriteLine("Done. Press ENTER to continue ...");
+            Console.ReadLine();
+
+        }
+    }
+}
