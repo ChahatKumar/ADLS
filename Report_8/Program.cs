ef9914ec50d100b918ec16c5f6555db94c0cd097
Chahat Kumar <kchahat20@gmail.com>
2020-07-07 02:37:36 +0530

    Updated :octocat:

--- Report_8/Program.cs
+++ Report_8/Program.cs
@@ -21,14 +21,22 @@ namespace Report_7
         private const int One_MB = 1050000;
         private const int Two_MB = 2100000;
 
-        private byte[][] buffer1 = new byte[5][];
-        private byte[][] buffer2 = new byte[5][];
+        // using blocking collections of type byte [] to act as a buffer pool with max capping of 5 for each 
+        // configuration ( 1mb , 2 mb, 4 mb)
+        private BlockingCollection<byte []> b1 = new BlockingCollection<byte []>(5);
+        private BlockingCollection<byte []> b2 = new BlockingCollection<byte []>(5);
 
-        private ArrayPool<byte> samePool = ArrayPool<byte>.Shared;
+        // blocking collection to synchronize various sections
+        private BlockingCollection<int> sync1 = new BlockingCollection<int>(1);
+        private BlockingCollection<int> sync2 = new BlockingCollection<int>(1);
 
-        public void SendData(AdlsClient c, int i,BlockingCollection<int> b,string filename, string path)
+        // total initialed is the number of arrays that have been allocated in memmory so far 
+        private int total_initialized_1MB  = 0;
+        private int total_initialized_2MB = 0;
+
+        //function to send data from path to target ( filename)
+        public void SendData(AdlsClient c,string filename, string path)
         {
-            i = i % 5; 
             FileInfo f = new FileInfo(path); 
             long length = f.Length;
 
@@ -37,58 +45,73 @@ namespace Report_7
                 // sending data via 1 MB buffer if data is less than/equal to 1 MB
                 if (length - One_MB <= 0)
                 {
-                    b.Add(1);
-                    if (buffer1[i] == null)
-                    { 
-                        buffer1[i] = samePool.Rent(One_MB); 
-                    }
-                    b.Take(); 
-                    length = length - buffer1[i].Length;
-                    lock (buffer1[i])
-                    {
-                        using (var file = new FileStream(path, FileMode.Open))
-                        { 
-                            file.Read(buffer1[i], 0, buffer1[i].Length); 
-                            c.ConcurrentAppend(filename, true, buffer1[i] , 0, (int) buffer1[i].Length); 
-                            Array.Clear(buffer1[i], 0, (int) buffer1[i].Length);
-                        }
-                    } 
-                }
-                //sending data via 2 MB buffer if data is more than 1 MB
-                else
-                {
-                    //sending data via 2 MB buffer
-                    b.Add(1);
-                    if (buffer2[i] == null)
-                    { 
-                        buffer2[i] = samePool.Rent(Two_MB); 
-                    }
-                    b.Take(); 
-                    length = length - buffer2[i].Length;
-                    lock (buffer2[i])
-                    {
-                        using (var file = new FileStream(path, FileMode.Open))
-                        { 
-                            file.Read(buffer2[i], 0, buffer2[i].Length); 
-                            c.ConcurrentAppend(filename, true, buffer2[i] , 0, (int) buffer2[i].Length); 
-                            Array.Clear(buffer2[i], 0, (int) buffer2[i].Length);
-                        }
-                    } 
+                    sync1.Add(1);
 
+                    byte[] buffer1;
+
+                    //if total initialised is less than 5 and buffer is empty -> we allocate new array
+                    if( total_initialized_1MB <=4 && b1.Count==0)
+                    {
+                          byte [] temp = new byte[One_MB];
+                          b1.Add(temp);
+                          total_initialized_1MB++;
+                        
+                    }                
+                   //taking array 
+                   buffer1 = b1.Take();
+                  
+                   sync1.Take();
+                 
+                   length = length - buffer1.Length;
+                   lock (buffer1)
+                      {
+                          using (var file = new FileStream(path, FileMode.Open))
+                          { 
+                            file.Read(buffer1,0, buffer1.Length); 
+                            c.ConcurrentAppend(filename, true, buffer1, 0, buffer1.Length); 
+                            Array.Clear(buffer1, 0, buffer1.Length);
+                          }
+                      } 
+                 //returning the used array back to pool
+                  b1.Add(buffer1);
+                    
+                }
+
+                //sending data via 2 MB buffer if data is more than 1 MB but less than/ equal to 2 MB
+                else 
+                {
+                  
+                     sync2.Add(1);
+
+                     byte[] buffer2;
+
+                     //if total initialised is less than 5 and buffer is empty -> we allocate new array
+                     if( total_initialized_2MB <=4 && b2.Count==0)
+                     {
+                          byte [] temp = new byte[Two_MB];
+                          b2.Add(temp);
+                          total_initialized_2MB++;  
+                     }                
+                     //taking array 
+                     buffer2 = b2.Take();
+                    
+                     sync2.Take();
+                                    
+                     length = length - buffer2.Length;
+                     lock (buffer2)
+                      {
+                          using (var file = new FileStream(path, FileMode.Open))
+                          { 
+                            file.Read(buffer2,0, buffer2.Length); 
+                            c.ConcurrentAppend(filename, true, buffer2, 0, buffer2.Length); 
+                            Array.Clear(buffer2, 0, buffer2.Length);
+                          }
+                      } 
+                     //returning the used array back to pool
+                     b2.Add(buffer2);
                 }
             }
-               
-        }
-
-        ~Buffering()
-        { 
-          //returning all rented arrays 
-          for (int i = 0; i < 5; i++)
-          { 
-              samePool.Return(buffer1[i]);
-              samePool.Return(buffer2[i]);
-          }
-        }
+         }
     }
     public class Program
     {
@@ -96,7 +119,7 @@ namespace Report_7
         private static string applicationId = "35fe4d8f-b30e-40ed-8cdf-fee0216569de";     // Also called client id
         private static string clientSecret = ".P0KipHqZAj18k-.t8_-WN4v~90.Jst08h";
         private static string tenantId = "72f988bf-86f1-41af-91ab-2d7cd011db47";
-        private static string adlsAccountFQDN = "marstest.azuredatalakestore.net";   // full account FQDN, not just the account name like example.azure.datalakestore.net
+        private static string adlsAccountFQDN = "marstest.azuredatalakestore.net";   // full account FQDN
                                                                                   
         public static void Main(string[] args)
         {
@@ -109,26 +132,23 @@ namespace Report_7
 
             try
             {
-                //file that wil be created and appended
-                string filename = @"Report_8.txt";
-                string[] path = new string[20];
-                BlockingCollection<int> b = new BlockingCollection<int>(1);
+                string filename = @"DataCheckAgain.txt";
+                string[] path = new string[30];
 
-                Parallel.For(0, 20, i => {
+                Parallel.For(0,20, i => {
 
                     if (i < 10)
                     {
-                        path[i] = @"C:\Users\kchah\OneDrive\Desktop\ADLS\Report_8\Input\1MB\" + (i + 1) + ".txt";
+                        path[i] = @"C:\Users\kchah\OneDrive\Desktop\InputFiles\1MB\" + (i + 1) + ".txt";
                     }
-                    else
+                    else 
                     {
-                        path[i] = @"C:\Users\kchah\OneDrive\Desktop\ADLS\Report_8\Input\2MB\" + ((i%10) + 1) + ".txt";
+                        path[i] = @"C:\Users\kchah\OneDrive\Desktop\InputFiles\2MB\" + ((i % 10) + 1) + ".txt";
                     }
-
-                    obj.SendData(client,i,b, filename, path[i]);
+                  
+                    obj.SendData(client, filename, path[i]);
 
                 });
-
             }
             catch (AdlsException e)
             {
@@ -137,7 +157,6 @@ namespace Report_7
 
             Console.WriteLine("Done. Press ENTER to continue ...");
             Console.ReadLine();
-
         }
     }
 }
