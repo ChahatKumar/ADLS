235c8deee24a560233ff5cfbd3effa5186260b26
Chahat Kumar <kchahat20@gmail.com>
2020-07-09 02:20:17 +0530

    :gem:

--- CreatePool/Program.cs
+++ CreatePool/Program.cs
@@ -1,7 +1,4 @@
-﻿/*
- *Read 30 files( 10 of 1 MB ,10 of 2 MB and 10 of 4 MB) in parallel with 5 buffers of 1MB,
- * 5 buffers of 2 MB and 5 buffers of 4 MB by creating a custom pool using concurrent append in one file. 		
- */
+﻿
 using System;
 using System.IO;
 using System.Text;
@@ -22,17 +19,22 @@ namespace Report_7
         private const int Two_MB = 2100000;
         private const int Four_MB = 4200000;
 
-        private byte[] buffer1;
-        private byte[] buffer2;
-        private byte[] buffer3;
+        private byte[][] buffer1 = new byte[5][];
+        private byte[][] buffer2 = new byte[5][];
+        private byte[][] buffer3 = new byte[5][];
 
-        //private ArrayPool<byte> samePool = ArrayPool<byte>.Shared;
         private ArrayPool<byte> Pool1 = ArrayPool<byte>.Create(One_MB,5);
         private ArrayPool<byte> Pool2 = ArrayPool<byte>.Create(Two_MB,5);
-        private ArrayPool<byte> Pool3 = ArrayPool<byte>.Create(Four_MB,5);
+        private ArrayPool<byte> Pool4 = ArrayPool<byte>.Create(Four_MB,5);
 
-        public void SendData(AdlsClient c,string filename, string path)
+
+        BlockingCollection<int> b1 = new BlockingCollection<Int32>(1);
+        BlockingCollection<int> b2 = new BlockingCollection<Int32>(1);
+        BlockingCollection<int> b3 = new BlockingCollection<Int32>(1);
+
+        public void SendData(AdlsClient c, int i,string filename, string path)
         {
+            i = i % 5; 
             FileInfo f = new FileInfo(path); 
             long length = f.Length;
 
@@ -41,63 +43,87 @@ namespace Report_7
                 // sending data via 1 MB buffer if data is less than/equal to 1 MB
                 if (length - One_MB <= 0)
                 {
-                   
-                    buffer1 = Pool1.Rent(One_MB); 
-         
-                    length = length - buffer1.Length;
-                    lock (buffer1)
+                    if (buffer1[i] == null)
+                    {
+                        b1.Add(1);
+                        if (buffer1[i] == null)
+                        { 
+                            buffer1[i] = Pool1.Rent(One_MB); 
+                            Console.WriteLine(buffer1[i].Length);
+                        }
+                        b1.Take(); 
+                    }
+                    length = length - buffer1[i].Length;
+                    lock (buffer1[i])
                     {
                         using (var file = new FileStream(path, FileMode.Open))
                         { 
-                            file.Read(buffer1, 0, buffer1.Length); 
-                            c.ConcurrentAppend(filename, true, buffer1, 0, (int) buffer1.Length);
-                            Array.Clear(buffer1, 0, (int) buffer1.Length);
+                            file.Read(buffer1[i], 0, buffer1[i].Length); 
+                            c.ConcurrentAppend(filename, true, buffer1[i] , 0, (int) buffer1[i].Length); 
+                            Array.Clear(buffer1[i], 0, (int) buffer1[i].Length);
                         }
                     }
-
-                    Pool1.Return(buffer1);
+                    Pool1.Return(buffer1[i]);
                 }
-                //sending data via 2 MB buffer if data is more than 1 MB but less than 2 MB
+                //sending data via 2 MB buffer if data is more than 1 MB but less than/ equal to 2 MB
                 else if(length - Two_MB <= 0)
                 {
-                 
-                    buffer2 = Pool2.Rent(Two_MB); 
-                 
-                    length = length - buffer2.Length;
-                    lock (buffer2)
+                    if (buffer2[i] == null)
+                    {
+                        b2.Add(1);
+                        if (buffer2[i] == null)
+                        { 
+                            buffer2[i] = Pool2.Rent(Two_MB); 
+                           Console.WriteLine(buffer2[i].Length);
+
+                        }
+                        b2.Take(); 
+                    }
+                    length = length - buffer2[i].Length;
+                    lock (buffer2[i])
                     {
                         using (var file = new FileStream(path, FileMode.Open))
                         { 
-                            file.Read(buffer2, 0, buffer2.Length); 
-                            c.ConcurrentAppend(filename, true, buffer2 , 0, (int) buffer2.Length); 
-                            Array.Clear(buffer2, 0, (int) buffer2.Length);
+                            file.Read(buffer2[i], 0, buffer2[i].Length); 
+                            c.ConcurrentAppend(filename, true, buffer2[i] , 0, (int) buffer2[i].Length); 
+                            Array.Clear(buffer2[i], 0, (int) buffer2[i].Length);
                         }
                     }
 
-                    Pool2.Return(buffer2);
+                    Pool2.Return(buffer2[i]);
                 }
                 //sending data via 4 MB if data is more than 2 MB
                 else
                 {
-                    
-                    buffer3= Pool3.Rent(Four_MB); 
+                    if (buffer3[i] == null)
+                    {
+                        b3.Add(1);
+                        if (buffer3[i] == null)
+                        { 
+                            buffer3[i] = Pool4.Rent(Four_MB); 
+                           Console.WriteLine(buffer3[i].Length);
 
-                    length = length - buffer3.Length;
-                    lock (buffer3)
+                        }
+                        b3.Take(); 
+                    }
+                    length = length - buffer3[i].Length;
+                    lock (buffer3[i])
                     {
                         using (var file = new FileStream(path, FileMode.Open))
                         { 
-                            file.Read(buffer3, 0, buffer3.Length); 
-                            c.ConcurrentAppend(filename, true, buffer3 , 0, (int) buffer3.Length); 
-                            Array.Clear(buffer3, 0, (int) buffer3.Length);
+
+                            file.Read(buffer3[i], 0, buffer3[i].Length); 
+                            c.ConcurrentAppend(filename, true, buffer3[i] , 0, (int) buffer3[i].Length); 
+                            Array.Clear(buffer3[i], 0, (int) buffer3[i].Length);
                         }
                     }
 
-                    Pool3.Return(buffer3);
+                    Pool4.Return(buffer3[i]);
                 }
             }
                
         }
+
     }
     public class Program
     {
@@ -118,29 +144,27 @@ namespace Report_7
 
             try
             {
-                //file that wil be created and appended
-                string filename = @"YOLO_Chahat20.txt";
+                string filename = @"A2-COE.txt";
                 string[] path = new string[30];
 
-                Parallel.For(0, 30, i => {
+                Parallel.For(0,5, i => {
 
                     if (i < 10)
                     {
-                        path[i] = @"C:\Users\kchah\OneDrive\Desktop\ADLS\Report_10\Input\1MB\" + (i + 1) + ".txt";
+                        path[i] = @"C:\Users\kchah\OneDrive\Desktop\InputFiles\input" + (i + 1) + ".json";
                     }
-                    else if(i < 20)
+                    else if (i < 20)
                     {
-                        path[i] = @"C:\Users\kchah\OneDrive\Desktop\ADLS\Report_10\Input\2MB\" + ((i%10) + 1) + ".txt";
+                        path[i] = @"C:\Users\kchah\OneDrive\Desktop\InputFiles\2MB\" + ((i % 10) + 1) + ".txt";
                     }
                     else
                     {
-                        path[i] = @"C:\Users\kchah\OneDrive\Desktop\ADLS\Report_10\Input\4MB\" + ((i%10) + 1) + ".txt";
+                        path[i] = @"C:\Users\kchah\OneDrive\Desktop\InputFiles\4MB\" + ((i % 10) + 1) + ".txt";
                     }
 
-                    obj.SendData(client,filename, path[i]);
+                    obj.SendData(client, i, filename, path[i]);
 
                 });
-
             }
             catch (AdlsException e)
             {
